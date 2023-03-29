af03a6a26965c9e689dcbc75520a2915fac30f27
Davi Borges <davifelipe_300@hotmail.com>
2023-03-29 20:43:48 -0300

    feat: exercicio tres

--- Exercicio06.ConsoleApp/Exercicio06.cs
+++ Exercicio06.ConsoleApp/Exercicio06.cs
@@ -10,7 +10,42 @@
          */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+            Console.WriteLine("Digite a inicial da cidade ou o estado");
+            string letraOuEstadoInputada = Console.ReadLine();
+            Console.WriteLine();
+            StreamReader streamReader = new StreamReader("C:\\AcademiaProgramador\\Atividades09\\Exercicio06.ConsoleApp\\docs\\cidades.csv");
+            streamReader.ReadLine();
+            if (letraOuEstadoInputada.Length == 1)
+            {
+                Console.WriteLine($"Cidades que iniciam com a letra {letraOuEstadoInputada}");
+            }
+            else
+            {
+                Console.WriteLine($"Cidades que pertencem ao estado {letraOuEstadoInputada}");
+            }
+            while (!streamReader.EndOfStream) 
+            {
+                string linha = streamReader.ReadLine();
+                string cidade = linha.Split(',')[2];
+                string cidadePorEstado = linha.Split(",")[3];
+                if (letraOuEstadoInputada.Length == 1)
+                {
+                    if (cidade.StartsWith(letraOuEstadoInputada))
+                    {
+                        Console.WriteLine(cidade);
+                    }
+                }
+                if(letraOuEstadoInputada.Length > 1)
+                {
+                    if(cidadePorEstado.Contains(letraOuEstadoInputada))
+                    {
+                        Console.WriteLine(cidade);
+                    }
+                }
+
+            }
+                Console.ReadLine();
         }
     }
 }
